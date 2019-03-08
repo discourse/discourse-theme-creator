cba7ebb5b8cb129e136ec83ab56994aa398cd4f2
David Taylor <david@taylorhq.com>
2019-03-08 10:01:59 +0000

    DEV: Correct rubocop offence

--- app/jobs/scheduled/cleanup_topics.rb
+++ app/jobs/scheduled/cleanup_topics.rb
@@ -4,9 +4,9 @@ module Jobs
 
     def execute(_args)
       Topic.where(category_id: SiteSetting.theme_creator_junk_category_ids.split('|'))
-           .where('bumped_at < ?', 1.week.ago)
-           .where(pinned_at: nil)
-           .each do |topic|
+        .where('bumped_at < ?', 1.week.ago)
+        .where(pinned_at: nil)
+        .each do |topic|
         PostDestroyer.new(Discourse.system_user, topic.first_post).destroy
       end
     end
