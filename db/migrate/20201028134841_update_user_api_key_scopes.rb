cea81ee0820d412ea5ea4dc7464cdb1fb228ed4c
David Taylor <david@taylorhq.com>
2020-10-28 20:43:45 +0000

    FIX: Update for changes to UserApiKeys in core (#13)

---
+++ db/migrate/20201028134841_update_user_api_key_scopes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UpdateUserApiKeyScopes < ActiveRecord::Migration[6.0]
+  def up
+    execute <<~SQL
+      UPDATE user_api_key_scopes
+      SET name = 'discourse-theme-creator:user_themes'
+      WHERE name = 'user_themes'
+    SQL
+  end
+end
