e3f051d0f5d85fab2b6b0faddef090c317c3bcd0
Mark VanLandingham <markvanlan@gmail.com>
2023-05-11 12:11:40 -0500

    FIX: Saving theme settings (#62)

--- assets/javascripts/discourse/components/user-theme-setting-editor.js
+++ assets/javascripts/discourse/components/user-theme-setting-editor.js
@@ -2,5 +2,5 @@ import ThemeSettingEditor from "admin/components/theme-setting-editor";
 import { url } from "discourse/lib/computed";
 
 export default class UserThemeSettingEditor extends ThemeSettingEditor {
-  updateUrl = url("model.id", "/user_themes/%@/setting");
+  @url("model.id", "/user_themes/%@/setting") updateUrl;
 }
