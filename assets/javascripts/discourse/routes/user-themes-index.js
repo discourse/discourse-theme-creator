de4c776e17f3b4f10769cc926bbfc19f8a7251af
Jarek Radosz <jarek@cvx.dev>
2025-05-28 18:10:58 +0200

    fix core regression

--- assets/javascripts/discourse/routes/user-themes-index.js
+++ assets/javascripts/discourse/routes/user-themes-index.js
@@ -3,6 +3,10 @@ import AdminCustomizeThemesIndex from "admin/routes/admin-customize-themes-index
 export default class UserThemesIndex extends AdminCustomizeThemesIndex {
   templateName = "adminCustomizeThemesIndex";
 
+  beforeModel() {
+    // override the redirect in core
+  }
+
   setupController() {
     super.setupController(...arguments);
     const parentController = this.controllerFor("user.themes");
