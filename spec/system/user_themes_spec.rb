8ff38f759cc1597362f4e13b4d5f28280e906890
David Taylor <david@taylorhq.com>
2025-10-14 20:52:21 +0100

    FIX: Index route import
    Since 0147d7965f64566caa749d3a3b3a5095af5bd3c6, the index route was causing an ajax request to the admin panel, which would 404 for non-admin users. This commit corrects the import, adds a spec for it, and fixes up the object_setting spec.

---
+++ spec/system/user_themes_spec.rb
@@ -0,0 +1,22 @@
+describe "User themes", type: :system do
+  before { enable_current_plugin }
+
+  it "can navigate to the user themes page and create a theme" do
+    user = Fabricate(:user)
+    sign_in(user)
+
+    visit "/u/#{user.username}"
+    find("a[href='/u/#{user.username}/themes']").click
+    find(".user-create-theme .btn-primary").click
+
+    find(".install-theme-content__theme-name").fill_in with: "My theme"
+    find(".d-modal__footer .btn-primary").click
+
+    try_until_success do
+      expect(Theme.last.user.id).to eq(user.id)
+    end
+
+    expect(page).to have_current_path("/u/#{user.username}/themes/#{Theme.last.id}")
+    expect(page).to have_css(".theme-controls")
+  end
+end
